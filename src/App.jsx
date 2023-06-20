298d9d2147d3a56062043284b14a061f78f955c1
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-06-20 19:46:13 +0100

    testing photos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import data from './data.json'
-import hope from '../assets/thumbnails/beyond-earth/regular/large.jpg';
 
 function App() {
   const [count, setCount] = useState(0)
